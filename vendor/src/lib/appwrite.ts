2478da53ea8617b0a533cbf95fa670ca6a20f22b
Almoraschool <websitealmora@gmail.com>
2026-04-04 14:42:21 +0530

    Work

--- vendor/src/lib/appwrite.ts
+++ vendor/src/lib/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, Storage, ID, Query } from 'appwrite';
 
-export const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1';
-export const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '67d8487c001878d65a25';
+export const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://sgp.cloud.appwrite.io/v1';
+export const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '69ae84bc001ca4edf8c2';
 
 const client = new Client()
     .setEndpoint(ENDPOINT)
@@ -11,9 +11,9 @@ export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
 
-export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || '67d853e50020a597e793';
-export const VENUES_COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID || '67d853f00030225d3090';
-export const LEADS_COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_LEADS_COLLECTION_ID || '67d9342c002bd33e4b7a';
+export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || '69c2305e000ecd6d04c1';
+export const VENUES_COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID || 'party-dial';
+export const LEADS_COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_LEADS_COLLECTION_ID || '69cf7b100035f0d02235';
 export const STORAGE_BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || 'venues_photos';
 
 export { client, ID, Query };
